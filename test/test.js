71b832c625bacc9e9be2fda24050ed3a89997e15
chenyong5 <chenyong5@xiaomi.com>
2018-05-28 14:29:39 +0800

    update

--- test/test.js
+++ test/test.js
@@ -7,11 +7,11 @@ let assert = chai.assert
 
 describe('compressfiles方法测试', () => {
   it('将文件压缩后放到dist目录', (done) => {
-    compressfiles('test/dist', 'test/case1', 'ignoredir')
+    compressfiles('test/dist', 'test/src', 'ignoredir')
     const excutePath = process.cwd()
     const f = () => {
-      const dirStat = fs.existsSync(path.join(excutePath, 'test/case1'))
-      const fileStat = fs.existsSync(path.join(excutePath, 'test/case1', 'index.js'))
+      const dirStat = fs.existsSync(path.join(excutePath, 'test/src'))
+      const fileStat = fs.existsSync(path.join(excutePath, 'test/src', 'index.js'))
       const ignoredirStat = fs.existsSync(path.join(excutePath, 'test/dist/ignoredir'))
 
       assert.strictEqual(dirStat, true, '创建dist目录')
