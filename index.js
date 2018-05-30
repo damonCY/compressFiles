792e0891d5e364c9830ae65a4d0af19bfe0a4076
chenyong5 <chenyong5@xiaomi.com>
2018-05-30 11:58:48 +0800

    update commander

--- index.js
+++ index.js
@@ -3,14 +3,37 @@
 const compressfiles = require('./lib/index.js')
 const program = require('commander')
 
-program.version('v' + require('./package.json').version)
-program.usage('<output> <source> <ignoredir>')
-  .alias('p')
-  .action(function (output, source, ignoredir) {
-    compressfiles(output, source, ignoredir)
+program
+  .usage('path [options]')
+  .action(function () {
+    init()
   })
-program.parse(process.argv)
+program
+  .version('v' + require('./package.json').version)
+  .option('-s, --source', 'Specify a folder which need to be minified.')
+  .option('-i, --ignore', 'Specify a folder which want to ignore.')
+  .parse(process.argv)
 
 if (program.args.length === 0) {
   program.help()
+}
+
+function init () {
+  const args = program.args
+  if (isEmpty(args[0])) {
+    console.log('Specify a file/folder to write the minified code')
+    return
+  }
+  if (isEmpty(args[1])) {
+    console.log('Specify a folder which need to be minified.')
+    return
+  }
+  compressfiles(args[0], args[1], args[2])
+}
+
+function isEmpty (str) {
+  if (typeof str !== 'string' || str.trim() === '') {
+    return true
+  }
+  return false
 }
